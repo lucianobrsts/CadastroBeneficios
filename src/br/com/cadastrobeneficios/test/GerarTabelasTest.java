954bd701515b7bba17d56688eb527985120d6bb4
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-05-07 10:22:16 -0300

    Adicionado imagem a principal do projeto

--- src/br/com/cadastrobeneficios/test/GerarTabelasTest.java
+++ src/br/com/cadastrobeneficios/test/GerarTabelasTest.java
@@ -1,5 +1,6 @@
 package br.com.cadastrobeneficios.test;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.cadastrobeneficios.util.HibernateUtil;
@@ -7,6 +8,7 @@ import br.com.cadastrobeneficios.util.HibernateUtil;
 public class GerarTabelasTest {
 
 	@Test
+	@Ignore
 	public void gerar() {
 		HibernateUtil.getSessionFactory();
 		HibernateUtil.getSessionFactory().close();
