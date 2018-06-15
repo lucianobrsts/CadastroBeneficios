555e1b73eda022586c66d71cf05e5c4baade63d6
Luciano Brito <Luciano Brito@lucianobrstsPC.home>
2018-06-15 18:47:02 -0300

    Finalizado criação dominio e db beneficio

--- src/br/com/cadastrobeneficios/domain/Beneficio.java
+++ src/br/com/cadastrobeneficios/domain/Beneficio.java
@@ -4,16 +4,18 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotEmpty;
 
 @Entity
-@Table(name = "beneficios")
+@Table(name = "beneficio")
 public class Beneficio {
 
 	@Id
@@ -21,78 +23,22 @@ public class Beneficio {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long codigo;
 
-	@NotEmpty(message = "O campo oficiona é obrigatório.")
-	@Column(name = "oficina", length = 50, nullable = false)
-	private String oficina;
-
-	@Column(name = "datacadastro", nullable = false)
+	@Column(name = "data_inicio", nullable = false)
 	@Temporal(value = TemporalType.TIMESTAMP)
-	private Date dataCadastro;
+	private Date dataInicio;
+
+	@Column(name = "data_fim", nullable = false)
+	@Temporal(value = TemporalType.TIMESTAMP)
+	private Date dataFim;
+
+	@Column(name = "dia_semana", nullable = false)
+	private String diaDaSemana;
 
 	@Column(name = "horario", length = 50, nullable = false)
 	private String horario;
 
-	public Long getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(Long codigo) {
-		this.codigo = codigo;
-	}
-
-	public String getOficina() {
-		return oficina;
-	}
-
-	public void setOficina(String oficina) {
-		this.oficina = oficina;
-	}
-
-	public Date getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(Date dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
-
-	public String getHorario() {
-		return horario;
-	}
-
-	public void setHorario(String horario) {
-		this.horario = horario;
-	}
-
-	@Override
-	public String toString() {
-		return "Beneficio [codigo=" + codigo + ", oficina=" + oficina + ", dataCadastro=" + dataCadastro + ", horario="
-				+ horario + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Beneficio other = (Beneficio) obj;
-		if (codigo == null) {
-			if (other.codigo != null)
-				return false;
-		} else if (!codigo.equals(other.codigo))
-			return false;
-		return true;
-	}
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "atividade_codigo", referencedColumnName = "codigo", nullable = false)
+	private Atividade atividade;
 
 }
